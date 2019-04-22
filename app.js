b9ff280364e0b4b9956b3e9eb1be6fafba6f0c79
Karthik <karthik.bala@sterlingadministration.com>
2019-04-22 13:58:58 +0530

    added the app.get('/', function(req, res){

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const mongoose = require('./config/database'); //database configuration
 const port = 8888
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// connection to mongodb
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+app.get('/', function(req, res){
+	res.json({"tutorial" : "Build REST API with node.js"});
+});
 
 app.listen(process.env.PORT || 8888, () => console.log(`Example app listening on port ${port}!`));
