91d10b0a815f0b9083ba2881c6962fd13a2f1cec
Karthik <karthik.bala@sterlingadministration.com>
2019-04-22 14:45:17 +0530

    get user details

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -11,6 +11,23 @@ module.exports = {
 				  	res.json({status: "success", message: "User added successfully!!!", data: null});
 				  
 				});
-	}
+	},
+
+	getAll: function(req, res, next) {
+		let usersList = [];
+
+		userModel.find({}, function(err, users){
+			if (err){
+				next(err);
+			} else{
+				for (let user of users) {
+					usersList.push({id: user._id, name: user.name, password: user.password});
+				}
+				res.json({status:"success", message: "Users list found!!!", data:{user: usersList}});
+							
+			}
+
+		});
+	},
 
 }					
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const userController = require('../api/controllers/users');
 
 
 router.post('/register', userController.create);
+router.get('/get', userController.getAll);
 
 module.exports = router;
