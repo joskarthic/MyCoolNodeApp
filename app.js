2d289815298e44eb06ad2f2738fc50bf6cfd0720
Karthik <karthik.bala@sterlingadministration.com>
2019-04-22 12:17:13 +0530

    hide the db conn fn

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('./config/database'); //database configuration
 const app = express();
 
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', function(req, res){
 	res.json({"tutorial" : "Build REST API with node.js"});
